6c5cf86aa34ce076d379e7a341327351fa8ea71d
Matthew Dolan <mdolan.code@gmail.com>
2021-11-10 10:36:24 -0800

    Initial Commit

---
+++ LoginRxSwiftMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginRxSwiftMVVM
+//
+//  Created by Matthew Dolan on 2021-11-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
