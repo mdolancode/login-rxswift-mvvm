cc83f0449fb4885675f1cab109b5c1d7fe73b957
Matthew Dolan <mdolan.code@gmail.com>
2021-11-10 14:14:46 -0800

    Create IBOutlets for username and password textFields, and login button. Create action for login button.

--- LoginRxSwiftMVVM/ViewController.swift
+++ LoginRxSwiftMVVM/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        print("login button pressed")
+    }
+    
 }
 
