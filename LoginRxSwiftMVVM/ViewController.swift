6b18abc26f17ea441d37ccbd814fd4260aa6bf79
Matthew Dolan <mdolan.code@gmail.com>
2021-11-13 20:20:39 -0800

    Instantiate loginViewModel and disposeBag. Create listener for username and password. Bind isValid observable to loginButton.

--- LoginRxSwiftMVVM/ViewController.swift
+++ LoginRxSwiftMVVM/ViewController.swift
@@ -10,6 +10,9 @@ import RxSwift
 import RxCocoa
 
 class ViewController: UIViewController {
+    
+    private let loginViewModel = LoginViewModel()
+    private let disposeBag = DisposeBag()
 
     @IBOutlet var usernameTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
@@ -17,7 +20,12 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        usernameTextField.rx.text.map { $0 ?? "" }.bind(to: loginViewModel.usernameTextPublishSubject).disposed(by: disposeBag)
+        passwordTextField.rx.text.map { $0 ?? "" }.bind(to: loginViewModel.passwordTextPublishSubject).disposed(by: disposeBag)
+        
+        loginViewModel.isValid().bind(to: loginButton.rx.isEnabled).disposed(by: disposeBag)
+        loginViewModel.isValid().map { $0 ? 1 : 0.1 }.bind(to: loginButton.rx.alpha).disposed(by: disposeBag)
     }
 
     @IBAction func loginButtonPressed(_ sender: UIButton) {
