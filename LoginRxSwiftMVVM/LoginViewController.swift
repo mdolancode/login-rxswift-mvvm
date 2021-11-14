49cbffeb4769bc87b781b504ce24bfd615aaa133
Matthew Dolan <mdolan.code@gmail.com>
2021-11-13 20:51:47 -0800

    Create functions for sendValueForLogin and loginButtonEnabled.

--- LoginRxSwiftMVVM/LoginViewController.swift
+++ LoginRxSwiftMVVM/LoginViewController.swift
@@ -13,7 +13,7 @@ class LoginViewController: UIViewController {
     
     private let loginViewModel = LoginViewModel()
     private let disposeBag = DisposeBag()
-
+    
     @IBOutlet var usernameTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     @IBOutlet var loginButton: UIButton!
@@ -21,15 +21,21 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        loginButtonEnabled()
+        checkValueForLoginButton()
+    }
+    
+    func sendValueForLogin() {
         usernameTextField.rx.text.map { $0 ?? "" }.bind(to: loginViewModel.usernameTextPublishSubject).disposed(by: disposeBag)
         passwordTextField.rx.text.map { $0 ?? "" }.bind(to: loginViewModel.passwordTextPublishSubject).disposed(by: disposeBag)
-        
+    }
+    
+    func loginButtonEnabled() {
         loginViewModel.isValid().bind(to: loginButton.rx.isEnabled).disposed(by: disposeBag)
         loginViewModel.isValid().map { $0 ? 1 : 0.1 }.bind(to: loginButton.rx.alpha).disposed(by: disposeBag)
     }
-
+    
     @IBAction func loginButtonPressed(_ sender: UIButton) {
         print("login button pressed")
     }
-    
 }
